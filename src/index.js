aaa04a4cc3bd5e153b3196f955a6bc72d754fefd
Satyam Singh Chauhan <107349203+saty953@users.noreply.github.com>
2022-12-21 22:00:27 +0530

    gg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+class Api{
+    constructor(url){
+        this.url=url;
+        this.method="GET"
+        this.#secure=url.startsWith("https")
+    }
 
+    isSecure(){
+        return this.#secure
+    }
+
+    updateUrl(url){
+        this.url=url;
+        this.#secure=url.startsWith("https")
+    }
+}
 
 module.exports = { API }
